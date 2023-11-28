3c1510eb499b07324832319b1283be6491e3d32b
Дмитрий Свечкарь <svechkar.dmitry@yandex.ru>
2023-11-28 22:16:38 +0300

    configure_django_import_for_CSV

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'api',
     'reviews',
     'users',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from import_export.admin import ImportExportActionModelAdmin
+
+from .models import (Title, Category, Genre, User, Review, Comment)
+
+admin.site.unregister(User)
+
+@admin.register(Title, Category, Genre, Review, Comment, User)
+class AdminPanel(ImportExportActionModelAdmin):
+    pass
