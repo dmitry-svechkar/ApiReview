4260687aaf0973bff41564a2912fdb6e280a1556
Дмитрий Свечкарь <svechkar.dmitry@yandex.ru>
2023-12-01 20:07:54 +0300

    small_fixes. config django-import-export lib

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from import_export.admin import ImportExportActionModelAdmin
 
-from .models import (Title, Category, Genre, User, Review, Comment)
+from .models import (Title, Category, Genre, Review, Comment)
 
-admin.site.unregister(User)
+# admin.site.unregister(User)
 
-@admin.register(Title, Category, Genre, Review, Comment, User)
+
+@admin.register(Title, Category, Genre, Review, Comment)
 class AdminPanel(ImportExportActionModelAdmin):
     pass
